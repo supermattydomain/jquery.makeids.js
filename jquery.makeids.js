dd19260b116bf12bbc46ff898128c276a4f42370
Matt <matt@supermatty.com>
2013-04-07 08:49:52 +0000

    Add inline documentation. Handle two calls within the same millisecond by using a running index as well as the milliseconds.
    git-svn-id: https://svn/svn/jquery.makeids.js@2859 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- jquery.makeids.js
+++ jquery.makeids.js
@@ -5,15 +5,18 @@
  * Ensures that each of the elements in the set has a unique ID.
  */
 (function($) {
-	var idCounter = new Date().getTime();
 	$.fn.makeIDs = function() {
 		this.each(function(index, elt) {
+			var i = 0;
 			if (elt.id) {
 				// Already has an ID
 			} else if (document.uniqueID) {
+				// Built-in magic window property
 				elt.id = document.uniqueID;
+			} else {
+				// Use time in milliseconds and running index to handle same-milliscond calls
+				elt.id = 'uniqueIdPrefix_' + (new Date().getTime()) + '_' + (i++);
 			}
-		    elt.id = 'uniqueIdPrefix_' + idCounter++;
 		});
 		return this;
 	};
